5f680549a38cf0499added219dd4093391487cfc
RawanElbanna <99205603+RawanElbanna@users.noreply.github.com>
2022-04-04 02:55:03 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
+(function () {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
 
+  var entry = 0;
+
+  function change() {
+  
+    if (entry == 0) {
+      document.getElementById("validationCustomUsername").placeholder = "email@example/com"; 
+      entry++;
+    }
+}
